da1c89f025ae717a64d0cf7faa3663941c998b81
TranTanDat1162 <dattt11062002@gmail.com>
2024-03-10 12:21:03 +0700

    Completing basic view for client and admin views
    - Like heading

---
+++ src/main/java/vn/uef/g2/foodlocation/controller/UserInfoController.java
@@ -0,0 +1,10 @@
+package vn.uef.g2.foodlocation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class UserInfoController {
+
+}
