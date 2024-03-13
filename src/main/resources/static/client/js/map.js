950d222ecab71e6237a7b3f815249e408d71f69e
LmTrieu <lmtrieu250902@gmail.com>
2024-03-13 15:03:00 +0700

    Setup_SearchBox

--- src/main/resources/static/client/js/map.js
+++ src/main/resources/static/client/js/map.js
@@ -71,7 +71,35 @@ async function initMap() {
         }
     });
     //------------------------------------------
+    var searchBox = new google.maps.places.SearchBox(document.getElementById('search-input'));
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(document.getElementById('search-input'));
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+        searchBox.set('map', null);
 
+        var places = searchBox.getPlaces();
+
+        var bounds = new google.maps.LatLngBounds();
+        var i, place;
+        for (i = 0; place = places[i]; i++) {
+            (function(place) {
+                var marker = new google.maps.Marker({
+
+                    position: place.geometry.location
+                });
+                marker.bindTo('map', searchBox, 'map');
+                google.maps.event.addListener(marker, 'map_changed', function() {
+                    if (!this.getMap()) {
+                        this.unbindAll();
+                    }
+                });
+                bounds.extend(place.geometry.location);
+            }(place));
+
+        }
+        map.fitBounds(bounds);
+        searchBox.set('map', map);
+        map.setZoom(Math.min(map.getZoom(),12));
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
