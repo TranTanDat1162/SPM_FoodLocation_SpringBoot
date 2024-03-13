cc10233c09b0c6dbae8df7b3ade7e5759cd2cba8
LmTrieu <lmtrieu250902@gmail.com>
2024-03-13 14:19:34 +0700

    Basic Register Form

--- src/main/java/vn/uef/g2/foodlocation/controller/client/ClientUserController.java
+++ src/main/java/vn/uef/g2/foodlocation/controller/client/ClientUserController.java
@@ -1,14 +1,32 @@
 package vn.uef.g2.foodlocation.controller.client;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import vn.uef.g2.foodlocation.domain.dto.SignUpDto;
+import vn.uef.g2.foodlocation.domain.dto.UserDto;
+import vn.uef.g2.foodlocation.domain.entity.User;
+import vn.uef.g2.foodlocation.repository.IRoleRepository;
+import vn.uef.g2.foodlocation.repository.IUserRepository;
 
 @Controller
 @RequiredArgsConstructor
 public class ClientUserController {
+    private final IRoleRepository roleRepository;
+    private final IUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @GetMapping("/login")
     public String login() {
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -24,8 +42,20 @@ public class ClientUserController {
 //        if (authentication != null && authentication.isAuthenticated() && !authentication.getPrincipal().equals("anonymousUser")) {
 //            return "redirect:/users";
 //        }
-//        model.addAttribute("user", new SignUpDto());
+
+        model.addAttribute("user", new SignUpDto());
         return "client/register";
+    }
+    @PostMapping("client/register")
+    public String registerUserAccount(@ModelAttribute("user") @Valid SignUpDto signUpDto,
+                                            HttpServletRequest request, Errors errors) {
+        User newUser = new User();
+        newUser.setEmail(signUpDto.getEmail());
+        newUser.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+        newUser.setFullName(signUpDto.getFullName());
+        newUser.setRole(roleRepository.findByRoleName("USER"));
+        userRepository.save(newUser);
+        return "redirect:/users";
     }
 
     @RequestMapping(value = {"/users","/users/"})
